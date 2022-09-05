3e808f0ce5db48ab442737112e149a5d4769b101
Aynur_Sonia <aynur@tkorp.com>
2022-09-05 21:13:42 +0200

    first commit

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-main>
+    </v-main>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: 'App',
+
+  mounted() {
+      axios.get("https://pokeapi.co/api/v2/pokemon/").then((response) => {
+        console.log(response.data)
+      })
+  }
+}
+
+</script>
